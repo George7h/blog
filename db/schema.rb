82b0e5b64be3e973e8c1b86b501da5bbad54833f
George <georgie.george7@gmail.com>
2024-01-11 12:12:22 +0400

    Update column names in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_10_064216) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_115811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.text "content"
+    t.text "text"
     t.bigint "user_id"
     t.bigint "post_id"
     t.datetime "created_at", null: false
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_064216) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.string "text"
     t.bigint "author_id"
     t.integer "comments_count", default: 0
     t.integer "likes_count", default: 0
