84900ec528aff23eee461045e8a075768e146051
George <georgie.george7@gmail.com>
2024-01-11 18:15:21 +0400

    Add after_create_commit callback to update posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,16 @@ class Post < ApplicationRecord
 
   has_many :likes, dependent: :destroy
 
-  def update_posts_counter
-    author.postsCounter += 1
-    author.save
-  end
+  after_create_commit :update_posts_counter
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.postsCounter += 1
+    author.save
   end
 end
