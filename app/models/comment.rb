182d4350063631f2851e4d161f0169a1a183c80e
George <georgie.george7@gmail.com>
2024-01-11 12:13:31 +0400

    Add associations and callback to update comments count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  after_create_commit :update_comments_counter
+
+  def update_comments_counter
+    post.update(comments_count: post.comments.count)
+  end
 end
