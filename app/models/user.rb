9f0976aa936d7b676c6c9ac9349f8ebd36e0f774
George <georgie.george7@gmail.com>
2024-01-11 12:12:55 +0400

    Add associations and methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
+  has_many :comments, dependent: :destroy
+
+  has_many :likes, dependent: :destroy
+
+  def update_posts_counter
+    postsCounter += 1
+    save
+  end
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
