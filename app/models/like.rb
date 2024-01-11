afc0b8e60ca5268a9a9f7f83906208cc22922760
George <georgie.george7@gmail.com>
2024-01-11 12:13:37 +0400

    Add after_create_commit callback to update likes counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,10 @@
 class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  after_create_commit :update_likes_counter
+
+  def update_likes_counter
+    post.update(likes_count: post.likes.count)
+  end
 end
