fee11c798be0f2b6d68e10a87976ba4273c5f61c
George <georgie.george7@gmail.com>
2024-01-11 12:13:27 +0400

    Add associations and methods to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name:'User'
+  has_many :comments, dependent: :destroy
+
+  has_many :likes, dependent: :destroy
+
+  def update_posts_counter
+    author.postsCounter += 1
+    author.save
+  end
+
+  
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
