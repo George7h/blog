f53f08ce05eedac28f70a58ae02a25c696e25264
George <georgie.george7@gmail.com>
2024-01-11 18:20:36 +0400

    correct linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id, dependent: :destroy
-  
+
   has_many :comments, dependent: :destroy
-  
+
   has_many :likes, dependent: :destroy
 
   def update_posts_counter
