17ac8eeb4632714dec6ffef0d12677eaa0c2e1d8
George <georgie.george7@gmail.com>
2024-01-12 16:31:54 +0400

    Add validations for name and postsCounter in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   has_many :likes, dependent: :destroy
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :postsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     postsCounter
