4ee20c87c8ee96ad717b91281fa2bc48c8065596
George <georgie.george7@gmail.com>
2024-01-19 16:20:41 +0400

    Add current_user to posts controller and update show action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,36 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
     @posts = @posts.paginate(page: params[:page], per_page: 3)
+    @current_user = current_user
   end
 
   def show
     @post = Post.includes(:author).find(params[:id])
+    @current_user = current_user
+    @post = Post.find(params[:id])
+    @user = User.find(@post.author_id)
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = @current_user
+    if @post.save
+      flash[:notice] = 'Post created successfully'
+      redirect_to user_posts_path(@current_user)
+    else
+      flash[:alert] = 'Post not created'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
