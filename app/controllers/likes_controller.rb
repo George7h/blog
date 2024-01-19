62581e981e2396e26b6dd940099bc1dbfa429686
George <georgie.george7@gmail.com>
2024-01-19 16:19:46 +0400

    Add LikesController with create action

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,19 @@
+class LikesController < ApplicationController
+  layout 'application'
+
+  def create
+    @like = Like.new(like_params)
+    if @like.save
+      flash[:notice] = 'Like created successfully'
+    else
+      flash[:alert] = 'Like not created'
+    end
+    redirect_to user_post_path(@current_user, @like.post)
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id, :post_id)
+  end
+end
