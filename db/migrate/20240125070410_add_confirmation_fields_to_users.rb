d10aa74530990fa1f274544e4afb353214a665e9
George <georgie.george7@gmail.com>
2024-01-25 11:07:05 +0400

    Add confirmable module to User model and create migrations for confirmation fields

---
+++ db/migrate/20240125070410_add_confirmation_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddConfirmationFieldsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+  end
+end
