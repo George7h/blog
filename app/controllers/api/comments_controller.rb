f959d41b8a5bfc748302ec4cb6c2712d108eab16
Haider <malikhaiderkhan81@gmail.com>
2024-01-26 23:02:48 +0500

    set api for comments in controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,34 @@
-class Api::CommentsController < ApplicationController
-end
+module Api
+    class CommentsController < ApplicationController
+      skip_before_action :authenticate_user!, only: %i[index create]
+      skip_before_action :verify_authenticity_token
+      before_action :set_user_post
+  
+      def index
+        @comments = @post.comments
+        render json: @comments
+      end
+  
+      def create
+        @comment = @post.comments.new(comment_params)
+        @comment.user = @user
+  
+        if @comment.save
+          render json: @comment, status: :created
+        else
+          render json: { error: 'Comment not created' }, status: :unprocessable_entity
+        end
+      end
+  
+      private
+  
+      def set_user_post
+        @user = User.find(params[:user_id])
+        @post = @user.posts.find(params[:post_id])
+      end
+  
+      def comment_params
+        params.require(:comment).permit(:text)
+      end
+    end
+  end
