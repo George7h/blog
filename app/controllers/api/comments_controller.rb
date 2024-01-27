d162ebd23d8e792f530704c7d653c3dec89f3806
George <georgie.george7@gmail.com>
2024-01-27 16:57:17 +0400

    Fix authentication bug in CommentsController

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -2,7 +2,7 @@ module Api
   class CommentsController < ApplicationController
     skip_before_action :authenticate_user!, only: %i[index create]
     skip_before_action :verify_authenticity_token
-    before_action :authenticate_user!, only: [:create]
+    # before_action :authenticate_user!, only: [:create]
     before_action :set_user_post
 
     def index
@@ -13,9 +13,10 @@ module Api
     end
 
     def create
-      @user = current_user
+      @user = User.find(params[:user_id])
       @post = @user.posts.find(params[:post_id])
       @comment = @post.comments.new(comment_params)
+      @comment.user = @user
 
       if @comment.save
         render json: @comment, status: :created
